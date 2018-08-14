8849f586afcb3469b6b0f9f00ce29f5aa393a392
Matheus Melo Antiquera <matheus.melo.a@hotmail.com>
2018-08-13 22:56:56 -0300

    primeiro commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,13 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  public carros;
   constructor(public navCtrl: NavController) {
-
+    this.carros = [
+      { nome: "Azera V6", preco: 85000 },
+      { nome: "Onix 1.6", preco: 35000 },
+      { nome: "Fiest 2.0", preco: 52000 },
+    ];
   }
 
 }
