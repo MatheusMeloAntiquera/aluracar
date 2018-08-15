7e0d1e93d73577e3d8707cb1723d5a3e6cc71079
Matheus Melo Antiquera <matheus.melo.a@hotmail.com>
2018-08-14 23:11:06 -0300

    Aula 2 - Consumindo a API do cliente

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,18 +1,52 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { Carro } from '../../interfaces/carro';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CarrosServiceProvider } from '../../providers/carros-service/carros-service';
+import { NavLifeCycles } from '../../utils/ionic/nav/nav-lifecycles';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-  public carros;
-  constructor(public navCtrl: NavController) {
-    this.carros = [
-      { nome: "Azera V6", preco: 85000 },
-      { nome: "Onix 1.6", preco: 35000 },
-      { nome: "Fiest 2.0", preco: 52000 },
-    ];
+export class HomePage implements NavLifeCycles {
+
+  public carros: Carro[];
+  public loading;
+
+  constructor(
+    public navCtrl: NavController,
+    private _loadingCtrl: LoadingController,
+    private _alertCrtl: AlertController,
+    private _carrosService: CarrosServiceProvider
+  ) { }
+
+  ionViewDidLoad() {
+    this.loading = this._loadingCtrl.create({
+      content: "Aguarde o carregamento dos carros..."
+    });
+
+    this.loading.present();
+
+    this._carrosService.lista().subscribe(
+      (carros) => {
+        this.carros = carros;
+        this.loading.dismiss();
+      },
+      (error: HttpErrorResponse) => {
+        console.log(`Codigo: ${error.status} - ${error.message}`);
+
+        this.loading.dismiss();
+        this._alertCrtl.create({
+          title: "Falha na conexão",
+          subTitle: "Não foi possível carregar a lista de carros. Tente novamente mais tarde!",
+          buttons: [
+            { text: "Ok" }
+          ]
+        }).present();
+
+      },
+    )
   }
 
 }
